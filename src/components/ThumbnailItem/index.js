c38511092d6c702664c03ba58562c1ca1855c7df
priyanshu9026 <singpriyasnshu22@gmail.com>
2024-02-22 14:30:02 +0000

    initial commit

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,25 @@
+// Write your code here.
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {imageDetails, isActive, setActiveThumbnailId} = props
+  const {thumbnailUrl, thumbnailAltText, id} = imageDetails
+  const ThumbnailClassName = isActive ? 'thumbnail active' : 'thumbnail'
+
+  const onClickThumbnail = () => {
+    setActiveThumbnailId(id)
+  }
+  return (
+    <li className="thumbnail-list-item">
+      <button type="button" className="btn" onClick={onClickThumbnail}>
+        <img
+          src={thumbnailUrl}
+          alt={thumbnailAltText}
+          className={ThumbnailClassName}
+        />
+      </button>
+    </li>
+  )
+}
+
+export default ThumbnailItem
